af3d8c8f1a7ab540b9656132b8563a2874d02a70
Ruba <53349790+rubaalamr@users.noreply.github.com>
2020-07-27 23:10:33 +0300

    Update urlChecker.js

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,6 +1,6 @@
 function checkForurl(inputText) {
     console.log("::: Running checkForurl :::", inputText);
-    let validurl = [(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*);
+    let validurl = `[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`;
 
     if(validurl.includes(inputText)) {
         console.log("correct url!")
